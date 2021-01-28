f32579af4c4fd7e59734f476059b176437f76c7a
Ertugrul.Kara <ertugrul.kara@logo.com.tr>
2021-01-28 20:35:20 +0300

    test dosyasi guncellendi

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import requests
 BASE= "http://127.0.0.1:5000/"
 
 word="Hello How Are You"
-response = requests.get(BASE+ "helloworld/" +word)
+response = requests.get(BASE+ "speak/" +word)
 
 print(response )
